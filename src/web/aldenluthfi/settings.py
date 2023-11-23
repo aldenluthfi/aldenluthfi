76ed92b045d55e9ae99bd1ef664bc6051d2dd6e4
Alden Luthfi <alden.luthfi@ui.ac.id>
2023-11-23 21:13:50 +0700

    Updated

--- src/web/aldenluthfi/settings.py
+++ src/web/aldenluthfi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4p*rx!(nj%vo+!psguy%j&ozhoq4ebxu1u-g5ot(51^*(azs9u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aldenluth.fi', 'www.aldenluth.fi', '127.0.0.1']
 
