22ab12d9bfe5be15e4fdc36d3c4718dffd33d8a9
Alden Luthfi <alden.luthfi@ui.ac.id>
2023-11-29 15:30:19 +0700

    Updated UI

---
+++ src/django-web/aldenluthfi/static/js/theme.js
@@ -0,0 +1,101 @@
+const dark = document.querySelector(".moon")
+const light = document.querySelector(".sun")
+const modeToggle = document.querySelector(".mode-toggle")
+const hue = document.querySelector(".hue-selector")
+
+var modeToggleClicked = 0
+var hueSelectorOpened = 0
+
+let theme = localStorage.getItem("theme")
+
+if (theme != null) {
+    document.documentElement.classList.add(theme);
+    if (theme.includes("-dark")) {
+        dark.classList.add("hidden")
+        light.classList.remove("hidden")
+        document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
+        document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
+    } else {
+        dark.classList.remove("hidden")
+        light.classList.add("hidden")
+        document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
+        document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
+    }
+} else {
+    document.documentElement.classList.add("neutral")
+    localStorage.setItem("theme", "neutral")
+    dark.classList.remove("hidden")
+    light.classList.add("hidden")
+}
+
+document.documentElement.addEventListener(
+    "click",
+    function (e) {
+        if (document.activeElement != modeToggle && document.activeElement != hue) {
+            document.activeElement.blur()
+        }
+
+        if (document.activeElement != modeToggle && document.activeElement != hue) {
+            modeToggleClicked = 0
+            hueSelectorOpened = 0
+        }
+    }
+)
+
+modeToggle.addEventListener(
+    "click",
+    function (e) {
+        if (document.body.clientWidth >= 1024 || modeToggleClicked == 1 || hueSelectorOpened == 1) {
+            current = document.documentElement.classList[document.documentElement.classList.length - 1]
+            if (current.includes("-dark")) {
+                hueClass = current.replace("-dark", "")
+                dark.classList.remove("hidden")
+                light.classList.add("hidden")
+                document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
+                document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
+            } else {
+                hueClass = current + "-dark"
+                dark.classList.add("hidden")
+                light.classList.remove("hidden")
+                document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
+                document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
+            }
+            document.documentElement.classList.remove(current)
+            document.documentElement.classList.add(hueClass)
+            localStorage.setItem("theme", hueClass)
+            modeToggleClicked = 0
+        } else {
+            modeToggleClicked = 1
+            hueSelectorOpened = 1
+        }
+    }
+)
+
+
+const hues = ["neutral", "rose", "red", "orange", "amber", "yellow", "lime", "green", "emerald", "teal", "cyan", "sky", "blue", "indigo", "violet", "purple", "fuchsia", "pink", "rose"]
+
+hues.forEach(hue => {
+    let hueButton = document.querySelector(`.hue-${hue}`)
+    hueButton.addEventListener(
+        "mousedown",
+        function (e) {
+            hueClass = `${hue}`
+            current = document.documentElement.classList[0]
+            if (current.includes("-dark")) {
+                hueClass = `${hue}-dark`
+                if (!current.includes("neutral")) {
+                    document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
+                    document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
+                }
+            } else {
+                if (!current.includes("neutral")) {
+                    document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
+                    document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
+                }
+            }
+            document.documentElement.classList.remove(current)
+            document.documentElement.classList.add(hueClass)
+            localStorage.setItem("theme", hueClass);
+        }
+    )
+})
