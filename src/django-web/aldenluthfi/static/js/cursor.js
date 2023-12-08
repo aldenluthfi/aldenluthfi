0ade1800afc89043e238c43144a821b687c0a88b
Alden Luthfi <alden.luthfi@ui.ac.id>
2023-12-08 18:48:19 +0700

    Updated

--- src/django-web/aldenluthfi/static/js/cursor.js
+++ src/django-web/aldenluthfi/static/js/cursor.js
@@ -1,61 +1,13 @@
-const cursorDot = document.querySelector(".cursor");
-const clickables = document.querySelectorAll("a, button, .clickable");
-var mouseDown = 0;
 
-window.addEventListener(
-    "mousedown",
-    function (e) {
-        let classList = cursorDot.classList
-        classList.remove('w-10')
-        classList.remove('h-10')
-        classList.remove('h-6')
-        classList.remove('w-6')
-        classList.remove('h-4')
-        classList.remove('w-4')
-        classList.remove('h-2')
-        classList.remove('w-2')
-        if (!(e.target.tagName == "A") && !(e.target.tagName == "BUTTON") && !(e.target.classList.contains("clickable"))) {
-            classList.add('h-6')
-            classList.add('w-6')
-        } else {
-            classList.add('h-2')
-            classList.add('w-2')
-        }
-        this.document.activeElement.blur();
-        mouseDown++;
-    }
-)
+function cursor() {
+    const cursorDot = document.querySelector(".cursor");
+    const clickables = document.querySelectorAll("a, button, .clickable");
+    var mouseDown = 0;
 
-window.addEventListener(
-    "mouseup",
-    function (e) {
-        let classList = cursorDot.classList
-        classList.remove('w-10')
-        classList.remove('h-10')
-        classList.remove('h-6')
-        classList.remove('w-6')
-        classList.remove('h-4')
-        classList.remove('w-4')
-        classList.remove('h-2')
-        classList.remove('w-2')
-        if (!(e.target.tagName == "A") && !(e.target.tagName == "BUTTON") && !(e.target.classList.contains("clickable"))) {
-            classList.add('w-10')
-            classList.add('h-10')
-        } else {
-            classList.add('h-4')
-            classList.add('w-4')
-        }
-        mouseDown--;
-    }
-)
-
-clickables.forEach(clickable => {
-    clickable.addEventListener(
-        "mouseover",
+    window.addEventListener(
+        "mousedown",
         function (e) {
             let classList = cursorDot.classList
-            classList.remove('bg-transparent')
-            classList.add('bg-accent')
             classList.remove('w-10')
             classList.remove('h-10')
             classList.remove('h-6')
@@ -64,53 +16,107 @@ clickables.forEach(clickable => {
             classList.remove('w-4')
             classList.remove('h-2')
             classList.remove('w-2')
-            if (mouseDown == 1) {
+            if (!(e.target.tagName == "A") && !(e.target.tagName == "BUTTON") && !(e.target.classList.contains("clickable"))) {
+                classList.add('h-6')
+                classList.add('w-6')
+            } else {
                 classList.add('h-2')
                 classList.add('w-2')
+            }
+            this.document.activeElement.blur();
+            mouseDown++;
+        }
+    )
+
+    window.addEventListener(
+        "mouseup",
+        function (e) {
+            let classList = cursorDot.classList
+            classList.remove('w-10')
+            classList.remove('h-10')
+            classList.remove('h-6')
+            classList.remove('w-6')
+            classList.remove('h-4')
+            classList.remove('w-4')
+            classList.remove('h-2')
+            classList.remove('w-2')
+            if (!(e.target.tagName == "A") && !(e.target.tagName == "BUTTON") && !(e.target.classList.contains("clickable"))) {
+                classList.add('w-10')
+                classList.add('h-10')
             } else {
                 classList.add('h-4')
                 classList.add('w-4')
             }
+            mouseDown--;
         }
     )
-    clickable.addEventListener(
-        "mouseleave",
-        function (e) {
-            let classList = cursorDot.classList
-            classList.add('bg-transparent')
-            classList.remove('bg-accent')
-            classList.remove('w-10')
-            classList.remove('h-10')
-            classList.remove('h-6')
-            classList.remove('w-6')
-            classList.remove('h-4')
-            classList.remove('w-4')
-            classList.remove('h-2')
-            classList.remove('w-2')
-            if (mouseDown == 1) {
-                classList.toggle('w-6')
-                classList.toggle('h-6')
-            } else {
-                classList.toggle('w-10')
-                classList.toggle('h-10')
+
+    clickables.forEach(clickable => {
+        clickable.addEventListener(
+            "mouseover",
+            function (e) {
+                let classList = cursorDot.classList
+                classList.remove('bg-transparent')
+                classList.add('bg-accent')
+                classList.remove('w-10')
+                classList.remove('h-10')
+                classList.remove('h-6')
+                classList.remove('w-6')
+                classList.remove('h-4')
+                classList.remove('w-4')
+                classList.remove('h-2')
+                classList.remove('w-2')
+                if (mouseDown == 1) {
+                    classList.add('h-2')
+                    classList.add('w-2')
+                } else {
+                    classList.add('h-4')
+                    classList.add('w-4')
+                }
             }
+        )
+        clickable.addEventListener(
+            "mouseleave",
+            function (e) {
+                let classList = cursorDot.classList
+                classList.add('bg-transparent')
+                classList.remove('bg-accent')
+                classList.remove('w-10')
+                classList.remove('h-10')
+                classList.remove('h-6')
+                classList.remove('w-6')
+                classList.remove('h-4')
+                classList.remove('w-4')
+                classList.remove('h-2')
+                classList.remove('w-2')
+                if (mouseDown == 1) {
+                    classList.toggle('w-6')
+                    classList.toggle('h-6')
+                } else {
+                    classList.toggle('w-10')
+                    classList.toggle('h-10')
+                }
+            }
+        )
+    });
+
+    window.addEventListener(
+        "mousemove",
+        function (e) {
+            if (document.documentElement.clientWidth >= 1024) {
+                cursorDot.classList.remove('hidden')
+            } else {
+                cursorDot.classList.add('hidden')
+            }
+
+            cursorDot.animate({
+                left: `${e.clientX}px`,
+                top: `${e.clientY}px`,
+            }, { duration: 500, fill: "forwards", easing: "cubic-bezier(0.3, 0.2, 0.2, 1.4)" });
         }
     )
-});
+}
 
-window.addEventListener(
-    "mousemove",
-    function (e) {
-        if (document.documentElement.clientWidth >= 1024) {
-            cursorDot.classList.remove('hidden')
-        } else {
-            cursorDot.classList.add('hidden')
-        }
+cursor()
 
-        cursorDot.animate({
-            left: `${e.clientX}px`,
-            top: `${e.clientY}px`,
-        }, { duration: 500, fill: "forwards", easing: "cubic-bezier(0.3, 0.2, 0.2, 1.4)" });
-    }
-)
-
+document.addEventListener("htmx:afterRequest", function (e) {cursor()})
--- src/django-web/aldenluthfi/static/js/theme.js
+++ src/django-web/aldenluthfi/static/js/theme.js
@@ -1,122 +1,129 @@
-const dark = document.querySelector(".moon")
-const light = document.querySelector(".sun")
-const modeToggle = document.querySelector(".mode-toggle")
-const hue = document.querySelector(".hue-selector")
 
-var iosDevice = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream && "matchMedia" in window
-var modeToggleClicked = 0
+function theme() {
+    const dark = document.querySelector(".moon")
+    const light = document.querySelector(".sun")
+    const modeToggle = document.querySelector(".mode-toggle")
+    const hue = document.querySelector(".hue-selector")
 
-let theme = localStorage.getItem("theme")
+    var iosDevice = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream && "matchMedia" in window
+    var modeToggleClicked = 0
 
-if (theme != null) {
-    document.documentElement.classList.add(theme);
-    if (theme.includes("-dark")) {
-        dark.classList.add("hidden")
-        light.classList.remove("hidden")
-        document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
-        document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
+    let theme = localStorage.getItem("theme")
+
+    if (theme != null) {
+        document.documentElement.classList.add(theme);
+        if (theme.includes("-dark")) {
+            dark.classList.add("hidden")
+            light.classList.remove("hidden")
+            document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
+            document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
+        } else {
+            dark.classList.remove("hidden")
+            light.classList.add("hidden")
+            document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
+            document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
+        }
     } else {
+        document.documentElement.classList.add("neutral")
+        localStorage.setItem("theme", "neutral")
         dark.classList.remove("hidden")
         light.classList.add("hidden")
-        document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
-        document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
     }
-} else {
-    document.documentElement.classList.add("neutral")
-    localStorage.setItem("theme", "neutral")
-    dark.classList.remove("hidden")
-    light.classList.add("hidden")
-}
 
-document.documentElement.addEventListener(
-    "click",
-    function (e) {
-        if (document.activeElement != modeToggle && document.activeElement != hue) {
-            document.activeElement.blur()
-            modeToggleClicked = 0
-        }
-    }
-)
-
-document.documentElement.addEventListener(
-    "mouseover",
-    function (e) {
-        if (document.documentElement.clientWidth >= 1024) {
-            document.activeElement.blur()
-        }
-    }
-)
-
-modeToggle.addEventListener(
-    "mousedown",
-    function (e) {
-        if (document.documentElement.clientWidth >= 1024 || modeToggleClicked == 1) {
-            current = document.documentElement.classList[document.documentElement.classList.length - 1]
-            if (current.includes("-dark")) {
-                hueClass = current.replace("-dark", "")
-                dark.classList.remove("hidden")
-                light.classList.add("hidden")
-                document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
-                document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
-            } else {
-                hueClass = current + "-dark"
-                dark.classList.add("hidden")
-                light.classList.remove("hidden")
-                document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
-                document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
+    document.documentElement.addEventListener(
+        "click",
+        function (e) {
+            if (document.activeElement != modeToggle && document.activeElement != hue) {
+                document.activeElement.blur()
+                modeToggleClicked = 0
             }
-            document.documentElement.classList.remove(current)
-            document.documentElement.classList.add(hueClass)
-            localStorage.setItem("theme", hueClass)
-        } else if (!iosDevice) {
-            modeToggleClicked = 1
         }
-    }
-)
+    )
 
-modeToggle.addEventListener(
-    "mouseleave",
-    function (e) {
-        if (document.documentElement.clientWidth < 1024 && iosDevice) {
-            modeToggleClicked = 0
+    document.documentElement.addEventListener(
+        "mouseover",
+        function (e) {
+            if (document.documentElement.clientWidth >= 1024) {
+                document.activeElement.blur()
+            }
         }
-    }
-)
+    )
 
-modeToggle.addEventListener(
-    "mouseover",
-    function (e) {
-        if (document.documentElement.clientWidth < 1024 && iosDevice) {
-            modeToggleClicked = 1
-        }
-    }
-)
-
-const hues = ["neutral", "rose", "red", "orange", "amber", "yellow", "lime", "green", "emerald", "teal", "cyan", "sky", "blue", "indigo", "violet", "purple", "fuchsia", "pink", "rose"]
-
-hues.forEach(hue => {
-    let hueButton = document.querySelector(`.hue-${hue}`)
-    hueButton.addEventListener(
+    modeToggle.addEventListener(
         "mousedown",
         function (e) {
-            hueClass = `${hue}`
-            current = document.documentElement.classList[0]
-            if (current.includes("-dark")) {
-                hueClass = `${hue}-dark`
-                if (!current.includes("neutral")) {
+            if (document.documentElement.clientWidth >= 1024 || modeToggleClicked == 1) {
+                current = document.documentElement.classList[document.documentElement.classList.length - 1]
+                if (current.includes("-dark")) {
+                    hueClass = current.replace("-dark", "")
+                    dark.classList.remove("hidden")
+                    light.classList.add("hidden")
+                    document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
+                    document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
+                } else {
+                    hueClass = current + "-dark"
+                    dark.classList.add("hidden")
+                    light.classList.remove("hidden")
                     document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
                     document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
                 }
-            } else {
-                if (!current.includes("neutral")) {
-                    document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
-                    document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
-                }
+                document.documentElement.classList.remove(current)
+                document.documentElement.classList.add(hueClass)
+                localStorage.setItem("theme", hueClass)
+            } else if (!iosDevice) {
+                modeToggleClicked = 1
             }
-            document.documentElement.classList.remove(current)
-            document.documentElement.classList.add(hueClass)
-            localStorage.setItem("theme", hueClass);
-            modeToggleClicked = 1
         }
     )
-})
+
+    modeToggle.addEventListener(
+        "mouseleave",
+        function (e) {
+            if (document.documentElement.clientWidth < 1024 && iosDevice) {
+                modeToggleClicked = 0
+            }
+        }
+    )
+
+    modeToggle.addEventListener(
+        "mouseover",
+        function (e) {
+            if (document.documentElement.clientWidth < 1024 && iosDevice) {
+                modeToggleClicked = 1
+            }
+        }
+    )
+
+    const hues = ["neutral", "rose", "red", "orange", "amber", "yellow", "lime", "green", "emerald", "teal", "cyan", "sky", "blue", "indigo", "violet", "purple", "fuchsia", "pink", "rose"]
+
+    hues.forEach(hue => {
+        let hueButton = document.querySelector(`.hue-${hue}`)
+        hueButton.addEventListener(
+            "mousedown",
+            function (e) {
+                hueClass = `${hue}`
+                current = document.documentElement.classList[0]
+                if (current.includes("-dark")) {
+                    hueClass = `${hue}-dark`
+                    if (!current.includes("neutral")) {
+                        document.querySelector(".neutral-icon").classList.add("fill-neutral-700")
+                        document.querySelector(".neutral-icon").classList.remove("fill-neutral-500")
+                    }
+                } else {
+                    if (!current.includes("neutral")) {
+                        document.querySelector(".neutral-icon").classList.remove("fill-neutral-700")
+                        document.querySelector(".neutral-icon").classList.add("fill-neutral-500")
+                    }
+                }
+                document.documentElement.classList.remove(current)
+                document.documentElement.classList.add(hueClass)
+                localStorage.setItem("theme", hueClass);
+                modeToggleClicked = 1
+            }
+        )
+    })
+}
+
+theme()
+
+document.addEventListener("htmx:afterRequest", function (e) {theme()})
