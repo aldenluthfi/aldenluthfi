de6c086b3283511e314fe090b8248db0e19dd466
aldenluthfi <alden.luthfi@ui.ac.id>
2023-12-12 15:22:40 +0700

    Updated Language and Header Logic

---
+++ src/django-web/aldenluthfi/static/js/language.js
@@ -0,0 +1,14 @@
+document.addEventListener("htmx:afterRequest", function (e) {
+    if (window.scrollY > 0) {
+        document.querySelector("header").classList.remove("shadow-glow");
+        document.querySelector("header").classList.remove("shadow-md");
+        if (localStorage.getItem("theme").includes("dark")) {
+            document.querySelector("header").classList.add("shadow-glow");
+        } else {
+            document.querySelector("header").classList.add("shadow-md");
+        }
+    } else {
+        document.querySelector("header").classList.remove("shadow-glow");
+        document.querySelector("header").classList.remove("shadow-md");
+    }
+})
