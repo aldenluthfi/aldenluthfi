70615c8744838ba11dc6336d1b3f4862afde3e88
Alden Luthfi <alden.luthfi@ui.ac.id>
2023-12-12 20:29:20 +0700

    Sidebar Shadows

--- src/django-web/aldenluthfi/static/js/sidebar.js
+++ src/django-web/aldenluthfi/static/js/sidebar.js
@@ -1,21 +1,21 @@
 document.querySelector(".burger").addEventListener("mouseover", function () {
-    document.querySelector(".sidebar").classList.remove("shadow-glow");
-    document.querySelector(".sidebar").classList.remove("shadow-md");
+    document.querySelector(".sidebar").classList.remove("shadow-glow-xl");
+    document.querySelector(".sidebar").classList.remove("shadow-xl");
     if (localStorage.getItem("theme").includes("dark")) {
-        document.querySelector(".sidebar").classList.add("shadow-glow");
+        document.querySelector(".sidebar").classList.add("shadow-glow-xl");
     } else {
-        document.querySelector(".sidebar").classList.add("shadow-md");
+        document.querySelector(".sidebar").classList.add("shadow-xl");
     }
 });
 
 document.addEventListener("htmx:afterRequest", function (e) {
     document.querySelector(".burger").addEventListener("mouseover", function () {
-        document.querySelector(".sidebar").classList.remove("shadow-glow");
-        document.querySelector(".sidebar").classList.remove("shadow-md");
+        document.querySelector(".sidebar").classList.remove("shadow-glow-xl");
+        document.querySelector(".sidebar").classList.remove("shadow-xl");
         if (localStorage.getItem("theme").includes("dark")) {
-            document.querySelector(".sidebar").classList.add("shadow-glow");
+            document.querySelector(".sidebar").classList.add("shadow-glow-xl");
         } else {
-            document.querySelector(".sidebar").classList.add("shadow-md");
+            document.querySelector(".sidebar").classList.add("shadow-xl");
         }
     });
 })
