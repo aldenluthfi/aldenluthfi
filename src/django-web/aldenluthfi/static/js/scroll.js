fa046e44cd488d8f026ed17d598a8454cf0eec71
Alden Luthfi <alden.luthfi@ui.ac.id>
2024-02-10 12:36:11 +0700

    Fixes

--- src/django-web/aldenluthfi/static/js/scroll.js
+++ src/django-web/aldenluthfi/static/js/scroll.js
@@ -1,22 +1,10 @@
 var x = 0
 var speed = Math.floor(document.documentElement.clientWidth / 500)
 
-function getTranslateX(element) {
-    const style = window.getComputedStyle(element)
-    const matrix = new DOMMatrixReadOnly(style.transform)
-    return matrix.m41
-}
-
-function inOutQuad(n) {
-    n *= 2;
-    if (n < 1) return 0.5 * n * n;
-    return - 0.5 * (--n * (n - 2) - 1);
-};
-
 function marquee() {
     var text = document.querySelector('.sliding')
 
-    x = getTranslateX(text) - speed
+    x = x - speed
 
     if (x <= -Math.floor(0.25 * text.getBoundingClientRect().width)) {
         x = -x + speed
