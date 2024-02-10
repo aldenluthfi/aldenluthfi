48194b7ce0da78a09fb007585a20da45a759a38b
Alden Luthfi <alden.luthfi@ui.ac.id>
2024-02-10 14:12:37 +0700

    Tweaks

--- src/django-web/aldenluthfi/static/js/scroll.js
+++ src/django-web/aldenluthfi/static/js/scroll.js
@@ -1,16 +1,23 @@
 var x = 0
 var speed = Math.floor(document.documentElement.clientWidth / 500)
 
+function getTranslateX(element) {
+    const style = window.getComputedStyle(element)
+    const matrix = new DOMMatrixReadOnly(style.transform)
+    return matrix.m41
+}
+
 function marquee() {
     var text = document.querySelector('.sliding')
 
-    x = x - speed
+    x = getTranslateX(text) - speed
 
     if (x <= -Math.floor(0.25 * text.getBoundingClientRect().width)) {
         x = -x + speed
     }
 
-    text.style.transform = `translateX(${x}px) skewX(0deg)`
+    text.style.transform = `translateX(${x}px)`
+    text.style.webkitTransform = `translateX(${x}px)`
 
     requestAnimationFrame(marquee)
 }
