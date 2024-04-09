cafe10b028e5439f643d41542e56fddf53350103
aldenluthfi <alden.luthfi@ui.ac.id>
2024-04-09 22:21:46 +0700

    Current Build

--- src/django-web/aldenluthfi/static/js/scroll.js
+++ src/django-web/aldenluthfi/static/js/scroll.js
@@ -1,5 +1,5 @@
 var text_x = 0
-var slideSpeed = 5
+var slideSpeed = 3
 
 function getTranslateX(element) {
     const style = window.getComputedStyle(element)
@@ -11,7 +11,7 @@ var oldMouseX = 0
 var mouseX = 0
 var deltaX = 0
 if (document.documentElement.clientWidth >= 1024) {
-    window.addEventListener('mousemove', function (e) {
+    document.querySelector('.title').addEventListener('mousemove', function (e) {
         mouseX = e.clientX
     })
 }
@@ -19,19 +19,31 @@ if (document.documentElement.clientWidth >= 1024) {
 function marquee() {
     var text = document.querySelector('.sliding')
 
-    text_x = getTranslateX(text) - slideSpeed - 5
-    slideSpeed = Math.max(Math.min(slideSpeed + (deltaX - slideSpeed) * 0.1, 15), -15)
+    text_x = getTranslateX(text) - slideSpeed - 3
+    if (deltaX > 30 || deltaX < -30) {
+        slideSpeed = Math.min(Math.max(slideSpeed + (deltaX - slideSpeed) * 0.1, -25), 15)
+    } else {
+        slideSpeed = slideSpeed * 0.95
+    }
 
-    if (text_x <= -Math.floor(0.25 * text.getBoundingClientRect().width)) {
-        text_x = -text_x + 5
+    if (text_x <= -Math.floor(0.25 * text.getBoundingClientRect().width) || text_x > Math.floor(0.25 * text.getBoundingClientRect().width)) {
+        text_x = -text_x
     }
 
     deltaX = mouseX - oldMouseX
     oldMouseX = mouseX
 
-    text.style.transform = `translate(${text_x}px, 0px) skewX(${Math.max(Math.min(slideSpeed, 8), -8)}deg)`
+    text.style.transform = `translate(${text_x}px, 0px) skewX(${Math.max(Math.min(slideSpeed + (deltaX - slideSpeed) * 0.1, 6), -8).toFixed(1)}deg)`
     window.requestAnimationFrame(marquee)
 }
 
 marquee()
+
+document.addEventListener("htmx:afterRequest", function (e) {
+    if (document.documentElement.clientWidth >= 1024) {
+        document.querySelector('.title').addEventListener('mousemove', function (e) {
+            mouseX = e.clientX
+        })
+    }
+})
 
